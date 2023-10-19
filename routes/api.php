d2b7d651d3f496098c8d0ad80ac4331546619918
Olsza <olsza@users.noreply.github.com>
2023-10-19 16:18:42 +0200

    add books Api Route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('books', BookController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
