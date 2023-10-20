be5c44124c08246b0468ab48ac49981468ecf650
Olsza <olsza@users.noreply.github.com>
2023-10-20 08:29:00 +0200

    transform using box view component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,116 +27,68 @@
 
         <div class="mt-16">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
-                <a href="https://laravel.com/docs"
-                   class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                    <div>
-                        <div
-                            class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                            <x-svg.book />
-                        </div>
+                <x-box
+                    title="Documentation"
+                    link="https://laravel.com/docs"
+                >
+                    Laravel has wonderful documentation covering every aspect of the framework. Whether you are
+                    a newcomer or have prior experience with Laravel, we recommend reading our documentation
+                    from beginning to end.
+                </x-box>
 
-                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
+                <x-box
+                    title="Laracasts"
+                    link="https://laracasts.com"
+                >
+                    Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development.
+                    Check them out, see for yourself, and massively level up your development skills in the
+                    process.
+                </x-box>
 
-                        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                            Laravel has wonderful documentation covering every aspect of the framework. Whether you are
-                            a newcomer or have prior experience with Laravel, we recommend reading our documentation
-                            from beginning to end.
-                        </p>
-                    </div>
+                <x-box
+                    title="Laravel News"
+                    link="https://laravel-news.com"
+                >
+                    Laravel News is a community driven portal and newsletter aggregating all of the latest and
+                    most important news in the Laravel ecosystem, including new package releases and tutorials.
+                </x-box>
 
-                    <x-svg.arrow-right />
-                </a>
+                <x-box title="Laravel News">
+                    Laravel's robust library of first-party tools and libraries, such as <a
+                        href="https://forge.laravel.com"
+                        class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Forge</a>,
+                    <a href="https://vapor.laravel.com"
+                       class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Vapor</a>,
+                    <a href="https://nova.laravel.com"
+                       class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Nova</a>,
+                    and <a href="https://envoyer.io"
+                           class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Envoyer</a>
+                    help you take your projects to the next level. Pair them with powerful open source libraries
+                    like <a href="https://laravel.com/docs/billing"
+                            class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Cashier</a>,
+                    <a href="https://laravel.com/docs/dusk"
+                       class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dusk</a>,
+                    <a href="https://laravel.com/docs/broadcasting"
+                       class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Echo</a>,
+                    <a href="https://laravel.com/docs/horizon"
+                       class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Horizon</a>,
+                    <a href="https://laravel.com/docs/sanctum"
+                       class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Sanctum</a>,
+                    <a href="https://laravel.com/docs/telescope"
+                       class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Telescope</a>,
+                    and more.
+                </x-box>
 
-                <a href="https://laracasts.com"
-                   class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                    <div>
-                        <div
-                            class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                            <x-svg.camera />
-                        </div>
-
-                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laracasts</h2>
-
-                        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                            Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development.
-                            Check them out, see for yourself, and massively level up your development skills in the
-                            process.
-                        </p>
-                    </div>
-
-                    <x-svg.arrow-right />
-                </a>
-
-                <a href="https://laravel-news.com"
-                   class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                    <div>
-                        <div
-                            class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                            <x-svg.document />
-                        </div>
-
-                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laravel News</h2>
-
-                        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                            Laravel News is a community driven portal and newsletter aggregating all of the latest and
-                            most important news in the Laravel ecosystem, including new package releases and tutorials.
-                        </p>
-                    </div>
-
-                    <x-svg.arrow-right />
-                </a>
-
-                <div
-                    class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                    <div>
-                        <div
-                            class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                            <x-svg.ecosystem-logo />
-                        </div>
-
-                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</h2>
-
-                        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                            Laravel's robust library of first-party tools and libraries, such as <a
-                                href="https://forge.laravel.com"
-                                class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Forge</a>,
-                            <a href="https://vapor.laravel.com"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Vapor</a>,
-                            <a href="https://nova.laravel.com"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Nova</a>,
-                            and <a href="https://envoyer.io"
-                                   class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Envoyer</a>
-                            help you take your projects to the next level. Pair them with powerful open source libraries
-                            like <a href="https://laravel.com/docs/billing"
-                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Cashier</a>,
-                            <a href="https://laravel.com/docs/dusk"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dusk</a>,
-                            <a href="https://laravel.com/docs/broadcasting"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Echo</a>,
-                            <a href="https://laravel.com/docs/horizon"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Horizon</a>,
-                            <a href="https://laravel.com/docs/sanctum"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Sanctum</a>,
-                            <a href="https://laravel.com/docs/telescope"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Telescope</a>,
-                            and more.
-                        </p>
-                    </div>
-                </div>
             </div>
         </div>
 
         <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
             <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
                 <div class="flex items-center gap-4">
-                    <a href="https://github.com/sponsors/taylorotwell"
+                    <a href="https://github.com/olsza/book-manager"
                        class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             class="-mt-px mr-1 w-5 h-5 stroke-gray-400 dark:stroke-gray-600 group-hover:stroke-gray-600 dark:group-hover:stroke-gray-400">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/>
-                        </svg>
-                        Sponsor
+                        <x-svg.heart />
+                        Code by Olsza
                     </a>
                 </div>
             </div>
