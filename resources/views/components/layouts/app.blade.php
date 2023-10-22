9fa782142925070b8f1a4f0d2c64927e6cf72e24
Olsza <olsza@users.noreply.github.com>
2023-10-22 22:54:59 +0200

    add new layout

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="antialiased">
+<div
+    class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+    @if (Route::has('login'))
+        <livewire:welcome.navigation/>
+    @endif
+
+    <div class="max-w-7xl mx-auto p-6 lg:p-8">
+        <div class="flex justify-center">
+            <x-svg.book w="full" h="20" />
+        </div>
+
+        <div class="mt-16">
+            {{ $slot }}
+        </div>
+
+        <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
+            <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
+                <div class="flex items-center gap-4">
+                    <a href="https://github.com/olsza/book-manager"
+                       class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                        <x-svg.heart />
+                        Code by Olsza
+                    </a>
+                </div>
+            </div>
+
+            <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+                Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
